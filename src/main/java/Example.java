fa38dcbf452f36e56b3f0dc20553ec20a703ceee
David Rodriguez Ayala <daroay@gmail.com>
2019-07-30 17:48:11 -0500

    Add jenkins file

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,6 +1,5 @@
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -9,7 +8,7 @@ public class Example {
 
   @RequestMapping("/")
   String home() {
-    return "Hello World - amigos!";
+    return "Hello and welcome!";
   }
 
   public static void main(String[] args) throws Exception {
