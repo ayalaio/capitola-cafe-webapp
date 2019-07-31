5bf192fe203958eca80a272f8a1822895f86bbf9
David Rodriguez Ayala <daroay@gmail.com>
2019-07-31 18:26:43 -0500

    Change in message

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -12,7 +12,7 @@ public class Example {
   }
 
   String message() {
-    return "Hello and DONE!";
+    return "Hello and Ready!";
   }
 
   public static void main(String[] args) throws Exception {
